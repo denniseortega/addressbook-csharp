c52bb784ceb07724c58423f1beeae5e972a7cfac
Dennise Ortega <dennise.i.ortega@gmail.com>
2018-04-20 12:42:50 -0700

    add initial contact class and details

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -7,3 +7,62 @@ namespace AddressBookApp.Models
 {
   public class Contact
   {
+    private string _name;
+    private int _number;
+    private string _address;
+    private int _id;
+    private static List<Contact> _contacts = new List<Contact>{};
+
+    public Contact(string name, int number, string address)
+    {
+      _name = name;
+      _number = number;
+      _address = address;
+      _contacts.Add(this);
+      _id = _contacts.Count;
+    }
+
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public int GetNumber()
+    {
+      return _number;
+    }
+    public void SetNumber(int newNumber)
+    {
+      _number = newNumber;
+    }
+    public string GetAddres()
+    {
+      return _address;
+    }
+
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _contacts;
+    }
+    public static void ClearAll()
+    {
+      _contacts.Clear();
+    }
+    public static Contact Find (int searchId)
+    {
+      return _contacts[searchId-1];
+    }
+  }
+}
